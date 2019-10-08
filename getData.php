e3465b5acd2ba19abbd4f975844b819a0d91691e
Phrawst <42211154+Phrawst@users.noreply.github.com>
2019-10-08 21:35:28 +0700

    Update getData.php

--- getData.php
+++ getData.php
@@ -1,11 +1,11 @@
 <?php 
 include 'config.php';
 
-$sel = mysqli_query($con,"select * from users");
+$sel = mysqli_query($con,"select * from อะไหล่ภายใน");
 $data = array();
 
 while ($row = mysqli_fetch_array($sel)) {
- $data[] = array("fname"=>$row['fname'],"lname"=>$row['lname'],"username"=>$row['username']);
+ $data[] = array("ชื่อสินค้า"=>$row['ชื่อสินค้า']);
 }
 echo json_encode($data);
 ?>
